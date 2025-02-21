ab61ada421379f3ce2bbc8967c3f05518aae2053
Ekaterina Zubareva <abouttkate@mail.ru>
2025-02-21 10:43:18 +0300

    Last commit

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,7 +1,16 @@
+/**
+ * SudokuSolver - класс для решения головоломки Судоку с использованием метода
+ *    проб и откатов (backtracking).
+ */
 public class SudokuSolver {
 
-    private static final int GRID_SIZE = 9; // Размер сетки судоку всегда 9x9
+    /** Размер сетки судоку (9x9). */
+    private static final int GRID_SIZE = 9;
 
+    /**
+     * Основной метод для запуска программы.
+     * @param args аргументы командной строки (не используются).
+     */
     public static void main(String[] args) {
         int[][] sudokuBoard = {
                 {4, 0, 9, 0, 2, 0, 0, 7, 1},
@@ -24,6 +33,10 @@ public class SudokuSolver {
         printBoard(sudokuBoard);
     }
 
+    /**
+     * Выводит текущее состояние судоку на консоль.
+     * @param board игровое поле 9x9.
+     */
     private static void printBoard(int[][] board) {
         for (int row = 0; row < GRID_SIZE; row++) {
             if (row % 3 == 0 && row != 0) {
@@ -39,6 +52,13 @@ public class SudokuSolver {
         }
     }
 
+    /**
+     * Проверяет, присутствует ли число в указанной строке.
+     * @param board игровое поле 9x9.
+     * @param number проверяемое число.
+     * @param row номер строки.
+     * @return true, если число уже есть в строке, иначе false.
+     */
     private static boolean isNumberInRow(int[][] board, int number, int row) {
         for (int i = 0; i < GRID_SIZE; i++) {
             if (board[row][i] == number) {
@@ -48,6 +68,13 @@ public class SudokuSolver {
         return false;
     }
 
+    /**
+     * Проверяет, присутствует ли число в указанном столбце.
+     * @param board игровое поле 9x9.
+     * @param number проверяемое число.
+     * @param column номер столбца.
+     * @return true, если число уже есть в столбце, иначе false.
+     */
     private static boolean isNumberInColumn(int[][] board, int number, int column) {
         for (int i = 0; i < GRID_SIZE; i++) {
             if (board[i][column] == number) {
@@ -57,6 +84,14 @@ public class SudokuSolver {
         return false;
     }
 
+    /**
+     * Проверяет, присутствует ли число в 3x3-блоке.
+     * @param board игровое поле 9x9.
+     * @param number проверяемое число.
+     * @param row номер строки.
+     * @param column номер столбца.
+     * @return true, если число уже есть в блоке, иначе false.
+     */
     private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
         int localBoxRow = row - row % 3;
         int localBoxColumn = column - column % 3;
@@ -71,12 +106,25 @@ public class SudokuSolver {
         return false;
     }
 
+    /**
+     * Проверяет, возможно ли поставить число в указанную клетку без нарушения правил судоку.
+     * @param board игровое поле 9x9.
+     * @param number проверяемое число.
+     * @param row номер строки.
+     * @param column номер столбца.
+     * @return true, если установка числа возможна, иначе false.
+     */
     private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
         return !isNumberInRow(board, number, row) &&
                 !isNumberInColumn(board, number, column) &&
                 !isNumberInBox(board, number, row, column);
     }
 
+    /**
+     * Решает судоку методом backtracking (проб и откатов).
+     * @param board игровое поле 9x9.
+     * @return true, если судоку можно решить, иначе false.
+     */
     private static boolean solveBoard(int[][] board) {
         for (int row = 0; row < GRID_SIZE; row++) {
             for (int column = 0; column < GRID_SIZE; column++) {
@@ -88,7 +136,7 @@ public class SudokuSolver {
                             if (solveBoard(board)) {
                                 return true;
                             } else {
-                                board[row][column] = 0; // Backtracking
+                                board[row][column] = 0; // Откат
                             }
                         }
                     }
@@ -96,6 +144,6 @@ public class SudokuSolver {
                 }
             }
         }
-        return true; // Доска решена
+        return true; // Судоку решено
     }
 }
